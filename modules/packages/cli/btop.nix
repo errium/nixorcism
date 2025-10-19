e88ec1d84fe4670adba97ac655a224a8d3fde80e
Errium <197423581+Errium@users.noreply.github.com>
2025-10-19 21:35:57 +0300

    Fixed btop

--- modules/packages/cli/btop.nix
+++ modules/packages/cli/btop.nix
@@ -1,12 +1,23 @@
-{pkgs, ...}: {
-  hm.programs.btop = {
-    enable = true;
-    package = pkgs.btop-cuda;
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}: {
+  options = {
+    nixDots.packages.cli.btop.enable = lib.mkEnableOption "Enables git";
+  };
 
-    settings = {
-      color_theme = "TTY";
-      theme_background = false;
-      rounded_corners = false;
+  config = lib.mkIf config.nixDots.packages.cli.btop.enable {
+    hm.programs.btop = {
+      enable = true;
+      package = pkgs.btop-cuda;
+
+      settings = {
+        color_theme = "TTY";
+        theme_background = false;
+        rounded_corners = false;
+      };
     };
   };
 }
--- modules/packages/cli/default.nix
+++ modules/packages/cli/default.nix
@@ -1,12 +1,15 @@
 {lib, ...}: {
   imports = [
     ./packages.nix
+
+    ./btop.nix
     ./git.nix
     ./helix.nix
     ./fastfetch.nix
   ];
 
   nixDots.packages.cli = lib.mkDefault {
+    btop.enable = true;
     git.enable = true;
     fastfetch.enable = true;
     helix.enable = true;
