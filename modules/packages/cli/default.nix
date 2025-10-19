4bf92b10ddb521ce2228b67072785dbaac370c94
Errium <197423581+Errium@users.noreply.github.com>
2025-10-19 19:21:55 +0300

    Custom fastfetch.enable option

--- modules/packages/cli/default.nix
+++ modules/packages/cli/default.nix
@@ -1,8 +1,12 @@
-{
+{lib, ...}: {
   imports = [
     ./packages.nix
     ./git.nix
     ./helix.nix
     ./fastfetch.nix
   ];
+
+  nixDots.packages.cli = lib.mkDefault {
+    fastfetch.enable = false;
+  };
 }
--- modules/packages/cli/fastfetch.nix
+++ modules/packages/cli/fastfetch.nix
@@ -1,127 +1,137 @@
 {
-  hm = {
-    programs.fastfetch = {
-      enable = true;
+  config,
+  lib,
+  ...
+}: {
+  options = {
+    nixDots.packages.cli.fastfetch.enable = lib.mkEnableOption "Enables fastfetch";
+  };
 
-      settings = {
-        logo = {
-          type = "auto";
-          position = "left";
-          # color = {
-          #   "1" = "blue";
-          #   "2" = "blue";
-          # };
+  config = lib.mkIf config.nixDots.packages.cli.fastfetch.enable {
+    hm = {
+      programs.fastfetch = {
+        enable = true;
+
+        settings = {
+          logo = {
+            type = "auto";
+            position = "left";
+            # color = {
+            #   "1" = "blue";
+            #   "2" = "blue";
+            # };
+          };
+
+          display = {
+            separator = "  ";
+            # color = {
+            #   keys = "blue";
+            # };
+          };
+
+          modules = [
+            {
+              key = "";
+              keyColor = "red";
+              type = "title";
+              format = "{user-name} @ {host-name}";
+            }
+
+            "custom"
+
+            {
+              key = "";
+              keyColor = "green";
+              type = "os";
+              format = "{pretty-name}";
+            }
+            {
+              key = "󰍹";
+              keyColor = "green";
+              type = "host";
+              format = "{family}";
+            }
+            {
+              key = "";
+              keyColor = "green";
+              type = "kernel";
+            }
+            {
+              key = "󰅕";
+              keyColor = "green";
+              type = "bootmgr";
+              format = "{name}";
+            }
+            {
+              key = "";
+              keyColor = "green";
+              type = "packages";
+            }
+
+            "custom"
+
+            {
+              key = "";
+              keyColor = "yellow";
+              type = "de";
+              format = "{pretty-name}";
+            }
+            {
+              key = "";
+              keyColor = "yellow";
+              type = "terminal";
+              format = "{pretty-name}";
+            }
+            {
+              key = "";
+              keyColor = "yellow";
+              type = "shell";
+            }
+
+            "custom"
+
+            {
+              key = "";
+              keyColor = "blue";
+              type = "cpu";
+              format = "{name} - {freq-max} {temperature}";
+              temp = true;
+            }
+            {
+              key = "";
+              keyColor = "blue";
+              type = "gpu";
+              format = "{name} {core-usage-num}";
+              driverSpecific = true;
+            }
+            {
+              key = "";
+              keyColor = "blue";
+              type = "memory";
+              format = "{used} / {total} {percentage}";
+            }
+            {
+              key = "󰾴";
+              keyColor = "blue";
+              type = "swap";
+              format = "{used} / {total} {percentage}";
+            }
+
+            "custom"
+
+            {
+              key = "";
+              keyColor = "magenta";
+              type = "disk";
+              format = "{days} days old";
+            }
+            {
+              key = "";
+              keyColor = "magenta";
+              type = "uptime";
+            }
+          ];
         };
-
-        display = {
-          separator = "  ";
-          # color = {
-          #   keys = "blue";
-          # };
-        };
-
-        modules = [
-          {
-            key = "";
-            keyColor = "red";
-            type = "title";
-            format = "{user-name} @ {host-name}";
-          }
-
-          "custom"
-
-          {
-            key = "";
-            keyColor = "green";
-            type = "os";
-            format = "{pretty-name}";
-          }
-          {
-            key = "󰍹";
-            keyColor = "green";
-            type = "host";
-            format = "{family}";
-          }
-          {
-            key = "";
-            keyColor = "green";
-            type = "kernel";
-          }
-          {
-            key = "󰅕";
-            keyColor = "green";
-            type = "bootmgr";
-            format = "{name}";
-          }
-          {
-            key = "";
-            keyColor = "green";
-            type = "packages";
-          }
-
-          "custom"
-
-          {
-            key = "";
-            keyColor = "yellow";
-            type = "de";
-            format = "{pretty-name}";
-          }
-          {
-            key = "";
-            keyColor = "yellow";
-            type = "terminal";
-            format = "{pretty-name}";
-          }
-          {
-            key = "";
-            keyColor = "yellow";
-            type = "shell";
-          }
-
-          "custom"
-
-          {
-            key = "";
-            keyColor = "blue";
-            type = "cpu";
-            format = "{name} - {freq-max} {temperature}";
-            temp = true;
-          }
-          {
-            key = "";
-            keyColor = "blue";
-            type = "gpu";
-            format = "{name} {core-usage-num}";
-            driverSpecific = true;
-          }
-          {
-            key = "";
-            keyColor = "blue";
-            type = "memory";
-            format = "{used} / {total} {percentage}";
-          }
-          {
-            key = "󰾴";
-            keyColor = "blue";
-            type = "swap";
-            format = "{used} / {total} {percentage}";
-          }
-
-          "custom"
-
-          {
-            key = "";
-            keyColor = "magenta";
-            type = "disk";
-            format = "{days} days old";
-          }
-          {
-            key = "";
-            keyColor = "magenta";
-            type = "uptime";
-          }
-        ];
       };
     };
   };
