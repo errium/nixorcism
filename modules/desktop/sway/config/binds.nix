f6c3808221b5bece38eb0a198cf3312de260e78d
Errium <197423581+Errium@users.noreply.github.com>
2025-10-22 14:52:32 +0300

    Sway binds

--- modules/desktop/sway/config/binds.nix
+++ modules/desktop/sway/config/binds.nix
@@ -1,11 +1,14 @@
-{
+{pkgs, ...}: {
   hm = {
     wayland.windowManager.sway.config = {
-      modifier = "Mod1";
+      modifier = "Mod4";
       left = "a";
       down = "s";
       up = "w";
       right = "d";
+
+      menu = "${pkgs.rofi}/bin/rofi -show drun --show-icons";
+      terminal = "${pkgs.alacritty}/bin/alacritty";
     };
   };
 }
