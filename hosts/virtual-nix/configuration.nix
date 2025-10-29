8480df8279893c4ca9baba0c828d0028dcc8a99d
Errium <197423581+Errium@users.noreply.github.com>
2025-10-29 07:07:15 +0200

    Portal changes for niri

--- hosts/virtual-nix/configuration.nix
+++ hosts/virtual-nix/configuration.nix
@@ -14,6 +14,7 @@
     sound.enable = true;
     xfce.enable = true;
     xkb.enable = true;
+    niri.enable = true;
   };
 
   nixorcism.packages = {
--- modules/desktop/niri/default.nix
+++ modules/desktop/niri/default.nix
@@ -16,6 +16,34 @@
 
     programs.niri = {
       enable = true;
+      package = pkgs.niri-unstable;
+    };
+
+    xdg.portal = {
+      enable = true;
+      config = {
+        common = {
+          default = "gtk gnome";
+          "org.freedesktop.impl.portal.ScreenCast" = "gnome";
+          "org.freedesktop.impl.portal.Screenshot" = "gnome";
+          "org.freedesktop.impl.portal.RemoteDesktop" = "gnome";
+        };
+        niri.default = [
+          "gtk"
+          "gnome"
+          "wlr"
+        ];
+
+        extraPortals = with pkgs; [
+          xdg-desktop-portal-gtk
+          xdg-desktop-portal-gnome
+          xdg-desktop-portal-wlr
+          xdg-desktop-portal
+        ];
+      };
+
+      systemd.user.services.xdg-desktop-portal.after = ["niri.service"];
+      systemd.user.services.xdg-desktop-portal-gnome.after = ["niri.service"];
     };
   };
 }
