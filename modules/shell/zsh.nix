b726f175a76e40cd2afd07568962c106709ebe60
Errium <197423581+Errium@users.noreply.github.com>
2025-11-04 22:14:22 +0200

    Add basic shell aliases, history config, and disable completion

--- modules/shell/zsh.nix
+++ modules/shell/zsh.nix
@@ -13,6 +13,37 @@
     hm.programs.zsh = {
       enable = true;
       zprof.enable = true;
+
+      shellAliases = let
+        confDir = "~/nixorcism";
+        sudo = "sudo";
+      in {
+        c = "clear";
+        ll = "eza -l";
+        la = "eza -a";
+        ff = "fastfetch";
+
+        nrs = "${sudo} nixos-rebuild switch --flake ${confDir}";
+        gen = "${sudo} nix-env -p /nix/var/nix/profiles/system --list-generations";
+        ngc = "${sudo} nix-collect-garbage -d";
+        upd = "nix flake update --flake ${confDir}";
+        upg = "${sudo} nixos-rebuild switch --upgrade --flake ${confDir}";
+      };
+
+      history = {
+        size = 5000;
+        save = 5000;
+        share = true;
+        append = true;
+        ignoreSpace = true;
+        ignoreAllDups = true;
+        ignoreDups = true;
+        saveNoDups = true;
+        findNoDups = true;
+        expireDuplicatesFirst = true;
+      };
+
+      enableCompletion = false;
     };
   };
 }
