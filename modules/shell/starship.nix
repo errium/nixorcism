0962d8f67cad3bcb24bc7825c63d7cdee8e972ea
Errium <197423581+Errium@users.noreply.github.com>
2025-11-05 12:29:46 +0200

    Add character symbols to Starship config

--- modules/shell/starship.nix
+++ modules/shell/starship.nix
@@ -10,6 +10,10 @@
       enable = true;
       settings = {
         add_newline = true;
+        character = {
+          success_symbol = "[>](bold green)";
+          error_symbol = "[>](bold red)";
+        };
       };
     };
   };
