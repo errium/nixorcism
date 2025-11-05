cd9a683a72e9fccfe5d61bb2e6dbf350ccae3a38
Errium <197423581+Errium@users.noreply.github.com>
2025-11-05 16:02:04 +0200

    Update bash with completion and custom prompt, add full Starship module configs

--- modules/shell/bash.nix
+++ modules/shell/bash.nix
@@ -8,12 +8,18 @@
   };
 
   config = lib.mkIf config.nixorcism.shell.bash.enable {
-    programs.bash.enable = true;
+    programs.bash = {
+      enable = true;
+      completion.enable = true;
 
-    hm = {
-      programs.bash = {
-        enable = true;
+      shellAliases = {
+        ll = "ls -l";
+        c = "clear";
       };
+
+      promptInit = ''
+        PS1='\[\e[1;2m\][\[\e[0m\] \[\e[93;1m\]\u\[\e[39m\]@\h\[\e[0m\] \[\e[2m\]in\[\e[0m\] \[\e[93;1m\]\w\[\e[0m\] \[\e[1;2m\]]\[\e[0m\] \[\e[1;2m\]\\$\[\e[0m\] '
+      '';
     };
   };
 }
