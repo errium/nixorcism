74a4097620f54a3519dc11ae74d2e02bf8725760
Errium <197423581+Errium@users.noreply.github.com>
2025-11-05 12:17:19 +0200

    Convert oh-my-posh settings from JSON to Nix syntax

--- modules/shell/oh-my-posh.nix
+++ modules/shell/oh-my-posh.nix
@@ -9,6 +9,27 @@
     hm.programs.oh-my-posh = {
       enable = true;
       settings = {
+        "$schema" = "https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/schema.json";
+        final_space = true;
+        version = 3;
+        blocks = [
+          {
+            type = "prompt";
+            alignment = "left";
+            segments = [
+              {
+                type = "path";
+                style = "powerline";
+                powerline_symbol = "\uE0B0";
+                foreground = "#ffffff";
+                background = "#61AFEF";
+                properties = {
+                  style = "folder";
+                };
+              }
+            ];
+          }
+        ];
       };
     };
   };
