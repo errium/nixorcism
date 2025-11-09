b614b66aa6b72a81bae6445ef94ab4eeda0e50cb
Errium <197423581+Errium@users.noreply.github.com>
2025-11-09 21:57:36 +0200

    fix: correct fastfetch logo color variable syntax

--- modules/packages/cli/fastfetch.nix
+++ modules/packages/cli/fastfetch.nix
@@ -8,152 +8,152 @@
   };
 
   config = lib.mkIf config.nixorcism.packages.cli.fastfetch.enable {
-    hm = {
-      programs.fastfetch = {
-        enable = true;
+    hm.programs.fastfetch = let
+      logoColor1 = "blue";
+      logoColor2 = "green";
+      keyColor1 = "green";
+      keyColor2 = "blue";
+    in {
+      enable = true;
 
-        settings = {
-          logo = {
-            type = "data";
-            source = ''
-              $1          ▗▄▄▄       $2.*#&.    ,~=,
-              $1          ▜███▙       $2&*+)^  %*^@*
-              $1           ▜███▙       $2>=-@\/%*$)
-              $1            ▜███▙       $2}(!-)$^]
-              $1     ▟█████████████████▙ $2&(#@)/     $1▟▙
-              $1    ▟███████████████████▙ $2$]<$\    $1▟██▙
-              $2           ,___,           !)::&  $1▟███▛
-              $2          [$*@#             %#!$ $1▟███▛
-              $2         &*%!^               \/ $1▟███▛
-              $2/)(*&!#$_#>)*                  $1▟██████████▙
-              $2\!@#*@#%<>?&                  $1▟███████████▛
-              $2      *}^#* $1▟▙               ▟███▛
-              $2     &[!@) $1▟██▙             ▟███▛
-              $2    /#$)$  $1▜███▙           ▝▀▀▀▀
-              $2    <][(    $1▜███▙ $2%&!~)$&!_)$*!@#$(<>/
-              $2     ^@     $1▟████▙ $2^?><!#$!(*&%!_%^)/
-              $1           ▟██████▙       $2*(!)\
-              $1          ▟███▛▜███▙       $2{>@%!
-              $1         ▟███▛  ▜███▙       $2&^#$|
-              $1         ▝▀▀▀    ▀▀▀▀▘       $2"*="
-            '';
-            position = "left";
-            color = {
-              "1" = "blue";
-              "2" = "green";
-            };
+      settings = {
+        logo = {
+          type = "data";
+          source = ''
+            $1          ▗▄▄▄       $2.*#&.    ,~=,
+            $1          ▜███▙       $2&*+)^  %*^@*
+            $1           ▜███▙       $2>=-@\/%*$)
+            $1            ▜███▙       $2}(!-)$^]
+            $1     ▟█████████████████▙ $2&(#@)/     $1▟▙
+            $1    ▟███████████████████▙ $2$]<$\    $1▟██▙
+            $2           ,___,           !)::&  $1▟███▛
+            $2          [$*@#             %#!$ $1▟███▛
+            $2         &*%!^               \/ $1▟███▛
+            $2/)(*&!#$_#>)*                  $1▟██████████▙
+            $2\!@#*@#%<>?&                  $1▟███████████▛
+            $2      *}^#* $1▟▙               ▟███▛
+            $2     &[!@) $1▟██▙             ▟███▛
+            $2    /#$)$  $1▜███▙           ▝▀▀▀▀
+            $2    <][(    $1▜███▙ $2%&!~)$&!_)$*!@#$(<>/
+            $2     ^@     $1▟████▙ $2^?><!#$!(*&%!_%^)/
+            $1           ▟██████▙       $2*(!)\
+            $1          ▟███▛▜███▙       $2{>@%!
+            $1         ▟███▛  ▜███▙       $2&^#$|
+            $1         ▝▀▀▀    ▀▀▀▀▘       $2"*="
+          '';
+          position = "left";
+          color = {
+            "1" = logoColor1;
+            "2" = logoColor2;
           };
-
-          display = {
-            separator = "  ";
-            # color = {
-            #   keys = "blue";
-            # };
-          };
-
-          modules = [
-            {
-              key = "";
-              keyColor = "red";
-              type = "title";
-              format = "{user-name} @ {host-name}";
-            }
-
-            "custom"
-
-            {
-              key = "";
-              keyColor = "green";
-              type = "os";
-              format = "{pretty-name}";
-            }
-            {
-              key = "󰍹";
-              keyColor = "green";
-              type = "host";
-              format = "{family}";
-            }
-            {
-              key = "";
-              keyColor = "green";
-              type = "kernel";
-            }
-            {
-              key = "󰅕";
-              keyColor = "green";
-              type = "bootmgr";
-              format = "{name}";
-            }
-            {
-              key = "";
-              keyColor = "green";
-              type = "packages";
-            }
-
-            "custom"
-
-            {
-              key = "";
-              keyColor = "yellow";
-              type = "de";
-              format = "{pretty-name}";
-            }
-            {
-              key = "";
-              keyColor = "yellow";
-              type = "terminal";
-              format = "{pretty-name}";
-            }
-            {
-              key = "";
-              keyColor = "yellow";
-              type = "shell";
-            }
-
-            "custom"
-
-            {
-              key = "";
-              keyColor = "blue";
-              type = "cpu";
-              format = "{name} - {freq-max} {temperature}";
-              temp = true;
-            }
-            {
-              key = "";
-              keyColor = "blue";
-              type = "gpu";
-              format = "{name} {core-usage-num}";
-              driverSpecific = true;
-            }
-            {
-              key = "";
-              keyColor = "blue";
-              type = "memory";
-              format = "{used} / {total} {percentage}";
-            }
-            {
-              key = "󰾴";
-              keyColor = "blue";
-              type = "swap";
-              format = "{used} / {total} {percentage}";
-            }
-
-            "custom"
-
-            {
-              key = "";
-              keyColor = "magenta";
-              type = "disk";
-              format = "{days} days old";
-            }
-            {
-              key = "";
-              keyColor = "magenta";
-              type = "uptime";
-            }
-          ];
         };
+
+        display = {
+          separator = "  ";
+        };
+
+        modules = [
+          {
+            key = "";
+            keyColor = keyColor1;
+            type = "title";
+            format = "{user-name} @ {host-name}";
+          }
+
+          "custom"
+
+          {
+            key = "";
+            keyColor = keyColor2;
+            type = "os";
+            format = "{pretty-name}";
+          }
+          {
+            key = "󰍹";
+            keyColor = keyColor2;
+            type = "host";
+            format = "{family}";
+          }
+          {
+            key = "";
+            keyColor = keyColor2;
+            type = "kernel";
+          }
+          {
+            key = "󰅕";
+            keyColor = keyColor2;
+            type = "bootmgr";
+            format = "{name}";
+          }
+          {
+            key = "";
+            keyColor = keyColor2;
+            type = "packages";
+          }
+
+          "custom"
+
+          {
+            key = "";
+            keyColor = keyColor1;
+            type = "de";
+            format = "{pretty-name}";
+          }
+          {
+            key = "";
+            keyColor = keyColor1;
+            type = "terminal";
+            format = "{pretty-name}";
+          }
+          {
+            key = "";
+            keyColor = keyColor1;
+            type = "shell";
+          }
+
+          "custom"
+
+          {
+            key = "";
+            keyColor = keyColor2;
+            type = "cpu";
+            format = "{name} - {freq-max} {temperature}";
+            temp = true;
+          }
+          {
+            key = "";
+            keyColor = keyColor2;
+            type = "gpu";
+            format = "{name} {core-usage-num}";
+            driverSpecific = true;
+          }
+          {
+            key = "";
+            keyColor = keyColor2;
+            type = "memory";
+            format = "{used} / {total} {percentage}";
+          }
+          {
+            key = "󰾴";
+            keyColor = keyColor2;
+            type = "swap";
+            format = "{used} / {total} {percentage}";
+          }
+
+          "custom"
+
+          {
+            key = "";
+            keyColor = keyColor1;
+            type = "disk";
+            format = "{days} days old";
+          }
+          {
+            key = "";
+            keyColor = keyColor1;
+            type = "uptime";
+          }
+        ];
       };
     };
   };
