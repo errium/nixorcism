97a8722cda3dca0b05db7ee1945ea88e0bf31bf0
Errium <197423581+Errium@users.noreply.github.com>
2025-11-14 16:34:57 +0200

    refactor: simplify git conditional email configuration
    - Remove complex platform abstraction in favor of simple includes
    - Add conditional email switching for GitLab and GitHub remotes
    - Set GitLab noreply email as default fallback

--- modules/packages/cli/git.nix
+++ modules/packages/cli/git.nix
@@ -10,46 +10,26 @@
   config = lib.mkIf config.nixorcism.packages.cli.git.enable {
     programs.git.enable = true;
 
-    hm.programs.git = let
-      mkGitInclude = platform: url: email: [
-        {
-          condition = "hasconfig:remote.*.url:https://${url}/**";
-          contents.user.email = email;
-        }
-        {
-          condition = "hasconfig:remote.*.url:git@${url}:*/**";
-          contents.user.email = email;
-        }
-      ];
-
-      platforms = {
-        github = {
-          url = "github.com";
-          email = "197423581+Errium@users.noreply.github.com";
-        };
-        gitlab = {
-          url = "gitlab.com";
-          email = "30936741-Errium@users.noreply.gitlab.com";
-        };
-      };
-    in {
+    hm.programs.git = {
       enable = true;
-
       settings = {
         user.name = "Errium";
-        user.email = platforms.gitlab.email;
+        user.email = "30936741-Errium@users.noreply.gitlab.com";
         init.defaultBranch = "main";
         pull.rebase = true;
         push.autoSetupRemote = true;
       };
 
-      includes = lib.flatten (
-        lib.mapAttrsToList (
-          name: cfg:
-            mkGitInclude name cfg.url cfg.email
-        )
-        platforms
-      );
+      includes = [
+        {
+          condition = "gitlab.com:*";
+          contents.user.email = "30936741-Errium@users.noreply.gitlab.com";
+        }
+        {
+          condition = "github.com:*";
+          contents.user.email = "197423581+Errium@users.noreply.github.com";
+        }
+      ];
     };
   };
 }
