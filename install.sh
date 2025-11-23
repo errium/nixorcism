e6399fef7872893d594f78ac11c174664505a946
Errium <197423581+Errium@users.noreply.github.com>
2025-11-23 11:21:49 +0200

    feat: add dynamic host discovery and interactive selection

--- install.sh
+++ install.sh
@@ -11,6 +11,9 @@ DIM="\033[2m"
 GRAY="\033[0;90m"
 BOLD_RED="\033[1;31m"
 BOLD_GREEN="\033[1;32m"
+BOLD_BLUE="\033[1;34m"
+BOLD_YELLOW="\033[1;33m"
+BOLD_MAGENTA="\033[1;35m"
 
 # ┏┓ ┏━┓┏┓╻┏┓╻┏━╸┏━┓
 # ┣┻┓┣━┫┃┗┫┃┗┫┣╸ ┣┳┛
@@ -56,6 +59,47 @@ check_git() {
 	fi
 }
 
+# ┏━┓┏━┓┏━┓┏┳┓┏━┓╺┳╸┏━┓
+# ┣━┛┣┳┛┃ ┃┃┃┃┣━┛ ┃ ┗━┓
+# ╹  ╹┗╸┗━┛╹ ╹╹   ╹ ┗━┛
+prompt_host() {
+	local hosts_dir="hosts"
+	local hosts=()
+
+	for dir in "$hosts_dir"/*; do
+		if [[ -d "$dir" ]] && [[ -f "$dir/configuration.nix" ]]; then
+			hosts+=("$(basename "$dir")")
+		fi
+	done
+
+	if [[ ${#hosts[@]} -eq 0 ]]; then
+		echo -e "${BOLD}[${BOLD_RED}FAILED${RESET}${BOLD}] No host configurations found"
+		exit 1
+	fi
+
+	echo -e "${BOLD}[${BOLD_MAGENTA}PROMPT${RESET}${BOLD}] Select hostname:"
+	for i in "${!hosts[@]}"; do
+		echo -e "$((i + 1)) - ${hosts[i]}"
+	done
+
+	while true; do
+		echo -e "${BOLD}Enter choice (1-${#hosts[@]}):${RESET}"
+		read -r choice
+
+		case "$choice" in
+		[0-9]*)
+			if ((choice >= 1 && choice <= ${#hosts[@]})); then
+				HOSTNAME="${hosts[choice - 1]}"
+				echo -e "${BOLD}[  ${BOLD_GREEN}OK${RESET}${BOLD}  ] Hostname set to: ${BOLD_MAGENTA}${HOSTNAME}"
+				break
+			fi
+			;;
+		esac
+
+		echo -e "${BOLD}[${BOLD_RED}FAILED${RESET}${BOLD}] Invalid choice"
+	done
+}
+
 # ┏━┓┏━┓┏━┓╺┳╸╻╺┳╸╻┏━┓┏┓╻╻┏┓╻┏━╸
 # ┣━┛┣━┫┣┳┛ ┃ ┃ ┃ ┃┃ ┃┃┗┫┃┃┗┫┃╺┓
 # ╹  ╹ ╹╹┗╸ ╹ ╹ ╹ ╹┗━┛╹ ╹╹╹ ╹┗━┛
@@ -64,6 +108,10 @@ check_git() {
 # ┃┃┗┫┗━┓ ┃ ┣━┫┃  ┃  ┣━┫ ┃ ┃┃ ┃┃┗┫
 # ╹╹ ╹┗━┛ ╹ ╹ ╹┗━╸┗━╸╹ ╹ ╹ ╹┗━┛╹ ╹
 
+# regen_hwconfig() {
+# 	nixos-generate-config --show-hardware-config >/mnt/etc/nixos/hosts/virtual-nix/hardware-configuration.nix
+# }
+
 # ┏━┓╻ ╻┏┓╻┏┓╻╻┏┓╻┏━╸
 # ┣┳┛┃ ┃┃┗┫┃┗┫┃┃┗┫┃╺┓
 # ╹┗╸┗━┛╹ ╹╹ ╹╹╹ ╹┗━┛
@@ -71,3 +119,4 @@ clear
 print_banner
 check_internet
 check_git
+prompt_host
