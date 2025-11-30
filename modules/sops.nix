5489a916e4453c337b8bc2eb55cbd3f6c4e4ab3b
Errium <197423581+Errium@users.noreply.github.com>
2025-11-30 11:03:15 +0200

    feat: add SSH key support to sops-nix configuration

--- modules/sops.nix
+++ modules/sops.nix
@@ -12,6 +12,20 @@
     secrets = {
       root-password.neededForUsers = true;
       user-password.neededForUsers = true;
+
+      ssh-private-key = {
+        owner = username;
+        group = "users";
+        mode = "0600";
+        path = "/home/${username}/.ssh/id_ed25519";
+      };
+
+      ssh-public-key = {
+        owner = username;
+        group = "users";
+        mode = "0644";
+        path = "/home/${username}/.ssh/id_ed25519.pub";
+      };
     };
   };
 }
