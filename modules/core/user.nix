42a160269b6a979b23fbcf2eeffd8c851cf3e422
Errium <197423581+Errium@users.noreply.github.com>
2025-12-14 22:53:28 +0200

    refactor: simplify user password configuration

--- modules/core/user.nix
+++ modules/core/user.nix
@@ -1,25 +1,22 @@
 {
   config,
-  lib,
   username,
   ...
 }: {
   users = {
     mutableUsers = false;
 
-    users = let
-      keyExists = builtins.pathExists "/home/${username}/.config/sops/age/keys.txt";
-    in {
+    users = {
       root = {
-        hashedPasswordFile = lib.mkIf keyExists config.sops.secrets.root-password.path;
-        initialHashedPassword = lib.mkIf (!keyExists) "$y$j9T$7uBk7C2pQGx3Wt0JxNB5W/$FtEF1HGlK.Qozr0E2tiLGu78X4ujtVyjfN8zVeTa0l5";
+        hashedPasswordFile = config.sops.secrets.root-password.path;
+        initialHashedPassword = "$y$j9T$7uBk7C2pQGx3Wt0JxNB5W/$FtEF1HGlK.Qozr0E2tiLGu78X4ujtVyjfN8zVeTa0l5";
       };
 
       ${username} = {
         isNormalUser = true;
         description = "Errium";
-        hashedPasswordFile = lib.mkIf keyExists config.sops.secrets.user-password.path;
-        initialHashedPassword = lib.mkIf (!keyExists) "$y$j9T$Hao7G9TGibBoEN6X1d2sW0$kemp6XaOm1RYTRWC4M0qvqJzTuwy2eO.wYpg3wQeQi5";
+        hashedPasswordFile = config.sops.secrets.user-password.path;
+        initialHashedPassword = "$y$j9T$Hao7G9TGibBoEN6X1d2sW0$kemp6XaOm1RYTRWC4M0qvqJzTuwy2eO.wYpg3wQeQi5";
 
         extraGroups = [
           "networkmanager"
