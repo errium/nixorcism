bc2353c266773b8615ec7a4cb69f1be7e884140f
Errium <197423581+Errium@users.noreply.github.com>
2025-12-15 13:12:01 +0200

    test: implement conditional password logic with keyExists

--- modules/core/user.nix
+++ modules/core/user.nix
@@ -6,27 +6,40 @@
   users = {
     mutableUsers = false;
 
-    users = {
-      root = {
-        hashedPasswordFile = config.sops.secrets.root-password.path;
-        # initialHashedPassword = "$y$j9T$7uBk7C2pQGx3Wt0JxNB5W/$FtEF1HGlK.Qozr0E2tiLGu78X4ujtVyjfN8zVeTa0l5";
-      };
+    users = let
+      keyExists = builtins.pathExists "/home/${username}/.config/sops/age/keys.txt";
+    in {
+      root =
+        if keyExists
+        then {
+          hashedPasswordFile = config.sops.secrets.root-password.path;
+        }
+        else {
+          initialHashedPassword = "$y$j9T$7uBk7C2pQGx3Wt0JxNB5W/$FtEF1HGlK.Qozr0E2tiLGu78X4ujtVyjfN8zVeTa0l5";
+        };
 
-      ${username} = {
-        isNormalUser = true;
-        description = "Errium";
-        hashedPasswordFile = config.sops.secrets.user-password.path;
-        # initialHashedPassword = "$y$j9T$Hao7G9TGibBoEN6X1d2sW0$kemp6XaOm1RYTRWC4M0qvqJzTuwy2eO.wYpg3wQeQi5";
-
-        extraGroups = [
-          "networkmanager"
-          "wheel"
-          "audio"
-          "sound"
-          "video"
-          "render"
-        ];
-      };
+      ${username} =
+        {
+          isNormalUser = true;
+          description = "Errium";
+          extraGroups = [
+            "networkmanager"
+            "wheel"
+            "audio"
+            "sound"
+            "video"
+            "render"
+          ];
+        }
+        // (
+          if keyExists
+          then {
+            hashedPasswordFile = config.sops.secrets.user-password.path;
+          }
+          else {
+            initialHashedPassword = "$y$j9T$Hao7G9TGibBoEN6X1d2sW0$kemp6XaOm1RYTRWC4M0qvqJzTuwy2eO.wYpg3wQeQi5";
+          }
+        );
     };
   };
 }
