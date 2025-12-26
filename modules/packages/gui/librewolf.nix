12893c75b81a3ea8aa37cc95e26d9b39520ebc7d
Errium <197423581+Errium@users.noreply.github.com>
2025-12-26 11:29:57 +0200

    feat: enhance librewolf with language packs and privacy policies

--- modules/packages/gui/librewolf.nix
+++ modules/packages/gui/librewolf.nix
@@ -10,6 +10,27 @@
   config = lib.mkIf config.nixorcism.packages.gui.librewolf.enable {
     hm.programs.librewolf = {
       enable = true;
+
+      languagePacks = [
+        "ru"
+        "en-US"
+      ];
+
+      policies = {
+        DisableTelemetry = true;
+        DisableFirefoxStudies = true;
+        DisablePocket = true;
+        DisableFormHistory = false;
+
+        DisplayBookmarksToolbar = "never";
+
+        ExtensionSettings = {
+          # "uBlock0@raymondhill.net" = {
+          #   install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+          #   installation_mode = "force_installed";
+          # };
+        };
+      };
     };
   };
 }
