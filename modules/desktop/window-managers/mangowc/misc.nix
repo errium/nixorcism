f868e325cb6889c0662b5903f1dc73684340fb0e
Errium <197423581+Errium@users.noreply.github.com>
2025-12-31 10:35:51 +0200

    feat: tidy mangowc misc settings

--- modules/desktop/window-managers/mangowc/misc.nix
+++ modules/desktop/window-managers/mangowc/misc.nix
@@ -1,19 +1,22 @@
 ''
   # Misc
-  no_border_when_single=0
   axis_bind_apply_timeout=100
-  focus_on_activate=1
-  idleinhibit_ignore_visible=0
-  sloppyfocus=1
-  warpcursor=1
-  focus_cross_monitor=0
-  focus_cross_tag=0
-  enable_floating_snap=0
-  snap_distance=30
   cursor_size=24
   drag_tile_to_tile=1
+  enable_floating_snap=0
+  exchange_cross_monitor=1
+  focus_cross_monitor=1
+  focus_cross_tag=1
+  focus_on_activate=1
+  idleinhibit_ignore_visible=0
+  no_border_when_single=0
+  sloppyfocus=1
+  snap_distance=30
+  syncobj_enable=1
+  view_current_to_back=0
+  warpcursor=1
 
-  # layer rule
+  # Layer rules
   layerrule=animation_type_open:zoom,layer_name:rofi
   layerrule=animation_type_close:zoom,layer_name:rofi
 ''
