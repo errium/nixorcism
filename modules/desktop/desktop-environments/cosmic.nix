be74b1aa0f78b1afd0ea822d2049ce6af44cf977
Errium <197423581+Errium@users.noreply.github.com>
2026-01-01 16:15:25 +0200

    feat: expand cosmic excluded packages

--- modules/desktop/desktop-environments/cosmic.nix
+++ modules/desktop/desktop-environments/cosmic.nix
@@ -15,7 +15,16 @@
     };
 
     environment.cosmic.excludePackages = with pkgs; [
+      alsa-utils
+      cosmic-edit
+      cosmic-initial-setup
       cosmic-player
+      cosmic-reader
+      cosmic-store
+      cosmic-term
+      cosmic-wallpapers
+      pulseaudio
+      xdg-user-dirs
     ];
   };
 }
