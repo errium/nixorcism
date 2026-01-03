858e0797aa099f78c50799600483acf37080f7ed
Errium <197423581+Errium@users.noreply.github.com>
2026-01-03 22:23:22 +0200

    fix: configure foot terminal font and server

--- modules/packages/gui/foot.nix
+++ modules/packages/gui/foot.nix
@@ -3,17 +3,18 @@
   lib,
   ...
 }: {
-  options.nixorcism.packages.gui = {
-    foot.enable = lib.mkEnableOption "Enables foot terminal";
+  options.nixorcism.packages.gui.foot = {
+    enable = lib.mkEnableOption "Whether to enable foot terminal";
   };
 
   config = lib.mkIf config.nixorcism.packages.gui.foot.enable {
     hm.programs.foot = {
       enable = true;
+      server.enable = true;
+
       settings = {
-        cursor = {
-          style = "beam";
-        };
+        main.font = config.nixorcism.font.mono + ":size=14";
+        cursor.style = "beam";
       };
     };
   };
