817beb91c537699996c2b40c942ced6e0bfb7558
Errium <197423581+Errium@users.noreply.github.com>
2026-01-03 15:33:02 +0200

    refactor: reorganize shell aliases

--- modules/shell/aliases.nix
+++ modules/shell/aliases.nix
@@ -5,24 +5,22 @@
 }: let
   confDir = "~/nixorcism";
   commonAliases = {
+    ff = "fastfetch";
     la = "eza -a";
     ll = "eza -lh";
     lla = "eza -lah";
-    ff = "fastfetch";
-
-    conf = "cd ${confDir}";
-
-    gen = "sudo nix-env -p /nix/var/nix/profiles/system --list-generations";
-    ngc = "sudo nix-collect-garbage -d";
-    nrs = "sudo nixos-rebuild switch --flake ${confDir}";
-    upd = "nix flake update --flake ${confDir}";
-    upg = "sudo nixos-rebuild switch --upgrade --flake ${confDir}";
+    # gen = "sudo nix-env -p /nix/var/nix/profiles/system --list-generations";
+    # ngc = "sudo nix-collect-garbage -d";
+    # nrs = "sudo nixos-rebuild switch --flake ${confDir}";
+    # upd = "nix flake update --flake ${confDir}";
+    # upg = "sudo nixos-rebuild switch --upgrade --flake ${confDir}";
   };
 in {
   options.nixorcism.shell.aliases = lib.mkOption {type = lib.types.attrs;};
 
   config = {
     environment.shellAliases = {
+      cf = "cd ${confDir}";
       la = "ls -a";
       ll = "ls -lh";
       lla = "ls -lah";
