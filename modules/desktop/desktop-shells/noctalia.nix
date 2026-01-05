8b3ed96305253a8af8798b4e736ab64337a6cd9a
Errium <197423581+Errium@users.noreply.github.com>
2026-01-05 17:05:10 +0200

    feat: enable stylix colors for noctalia shell

--- modules/desktop/desktop-shells/noctalia.nix
+++ modules/desktop/desktop-shells/noctalia.nix
@@ -13,24 +13,24 @@
       imports = [inputs.noctalia.homeModules.default];
       programs.noctalia-shell = {
         enable = true;
-        # colors = {
-        #   mError = "#dddddd";
-        #   mOnError = "#111111";
-        #   mOnPrimary = "#111111";
-        #   mOnSecondary = "#111111";
-        #   mOnSurface = "#828282";
-        #   mOnSurfaceVariant = "#5d5d5d";
-        #   mOnTertiary = "#111111";
-        #   mOnHover = "#ffffff";
-        #   mOutline = "#3c3c3c";
-        #   mPrimary = "#aaaaaa";
-        #   mSecondary = "#a7a7a7";
-        #   mShadow = "#000000";
-        #   mSurface = "#111111";
-        #   mHover = "#1f1f1f";
-        #   mSurfaceVariant = "#191919";
-        #   mTertiary = "#cccccc";
-        # };
+        colors = with config.lib.stylix.colors.withHashtag; {
+          mPrimary = base05;
+          mOnPrimary = base00;
+          mSecondary = base05;
+          mOnSecondary = base00;
+          mTertiary = base04;
+          mOnTertiary = base00;
+          mError = base08;
+          mOnError = base00;
+          mSurface = base00;
+          mOnSurface = base05;
+          mHover = base04;
+          mOnHover = base00;
+          mSurfaceVariant = base01;
+          mOnSurfaceVariant = base04;
+          mOutline = base02;
+          mShadow = base00;
+        };
       };
     };
   };
