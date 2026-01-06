d9518951959ab339683fb2bdc1fb803b7233a47d
Errium <197423581+Errium@users.noreply.github.com>
2026-01-06 07:00:44 +0200

    feat: add cursor and GTK icon theme configuration

--- modules/misc/styling/default.nix
+++ modules/misc/styling/default.nix
@@ -1,4 +1,9 @@
-{lib, ...}: {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   imports = [
     ./fonts.nix
     ./stylix.nix
@@ -7,5 +12,22 @@
 
   options.nixorcism.misc.styling = {
     enable = lib.mkEnableOption "system styling";
+  };
+
+  config = lib.mkIf config.nixorcism.misc.styling.enable {
+    hm = {
+      home.pointerCursor = {
+        name = "Bibata-Modern-Ice";
+        package = pkgs.bibata-cursors;
+        size = 24;
+        gtk.enable = true;
+        x11.enable = true;
+      };
+
+      gtk = {
+        iconTheme.name = "Adwaita";
+        iconTheme.package = pkgs.adwaita-icon-theme;
+      };
+    };
   };
 }
