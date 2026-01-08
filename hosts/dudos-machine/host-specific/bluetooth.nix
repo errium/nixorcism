468054e93dcb9379b4fbbe1b512f597ecaaf224f
Errium <197423581+Errium@users.noreply.github.com>
2026-01-08 17:50:43 +0200

    feat: add Bluetooth configuration for dudos-machine

---
+++ hosts/dudos-machine/host-specific/bluetooth.nix
@@ -0,0 +1,12 @@
+{
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = false;
+    settings = {
+      General = {
+        Experimental = true;
+        FastConnectable = false;
+      };
+    };
+  };
+}
