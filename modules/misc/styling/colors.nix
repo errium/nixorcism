1c0fa768877fcb4808d10eec2156e392a52a067c
Errium <197423581+Errium@users.noreply.github.com>
2026-01-17 16:32:31 +0200

    feat: add more color palettes and switch to vague

--- modules/misc/styling/colors.nix
+++ modules/misc/styling/colors.nix
@@ -1,24 +1,5 @@
 rec {
-  palette = everforest;
-
-  vague = {
-    base00 = "#141415";
-    base01 = "#252530";
-    base02 = "#606079";
-    base03 = "#606079";
-    base04 = "#aeaed1";
-    base05 = "#cdcdcd";
-    base06 = "#d7d7d7";
-    base07 = "#d7d7d7";
-    base08 = "#d8647e";
-    base09 = "#f3be7c";
-    base0A = "#f5cb96";
-    base0B = "#7fa563";
-    base0C = "#aeaed1";
-    base0D = "#6e94b2";
-    base0E = "#bb9dbd";
-    base0F = "#e08398";
-  };
+  palette = vague;
 
   everforest = {
     base00 = "#272e33";
@@ -39,6 +20,44 @@ rec {
     base0F = "#9da9a0";
   };
 
+  gruvbox = {
+    base00 = "#1d2021";
+    base01 = "#3c3836";
+    base02 = "#504945";
+    base03 = "#665c54";
+    base04 = "#bdae93";
+    base05 = "#d5c4a1";
+    base06 = "#ebdbb2";
+    base07 = "#fbf1c7";
+    base08 = "#fb4934";
+    base09 = "#fe8019";
+    base0A = "#fabd2f";
+    base0B = "#b8bb26";
+    base0C = "#8ec07c";
+    base0D = "#83a598";
+    base0E = "#d3869b";
+    base0F = "#d65d0e";
+  };
+
+  nord = {
+    base00 = "#2e3440";
+    base01 = "#3b4252";
+    base02 = "#434c5e";
+    base03 = "#4c566a";
+    base04 = "#d8dee9";
+    base05 = "#e5e9f0";
+    base06 = "#eceff4";
+    base07 = "#8fbcbb";
+    base08 = "#bd616a";
+    base09 = "#d08770";
+    base0A = "#ebcb8b";
+    base0B = "#a3be8c";
+    base0C = "#88c0d0";
+    base0D = "#81a1c1";
+    base0E = "#b48ead";
+    base0F = "#5e81ac";
+  };
+
   oxocarbon = {
     base00 = "#161616";
     base01 = "#262626";
@@ -56,5 +75,43 @@ rec {
     base0D = "#33b1ff";
     base0E = "#be95ff";
     base0F = "#3ddbd9";
+  };
+
+  tokyo-night = {
+    base00 = "#1a1b26";
+    base01 = "#16161e";
+    base02 = "#2f3549";
+    base03 = "#444b6a";
+    base04 = "#787c99";
+    base05 = "#a9b1d6";
+    base06 = "#cbccd1";
+    base07 = "#d5d6db";
+    base08 = "#c0caf5";
+    base09 = "#a9b1d6";
+    base0A = "#0db9d7";
+    base0B = "#9ece6a";
+    base0C = "#b4f9f8";
+    base0D = "#2ac3de";
+    base0E = "#bb9af7";
+    base0F = "#f7768e";
+  };
+
+  vague = {
+    base00 = "#141415";
+    base01 = "#252530";
+    base02 = "#606079";
+    base03 = "#606079";
+    base04 = "#aeaed1";
+    base05 = "#cdcdcd";
+    base06 = "#d7d7d7";
+    base07 = "#d7d7d7";
+    base08 = "#d8647e";
+    base09 = "#f3be7c";
+    base0A = "#f5cb96";
+    base0B = "#7fa563";
+    base0C = "#aeaed1";
+    base0D = "#6e94b2";
+    base0E = "#bb9dbd";
+    base0F = "#e08398";
   };
 }
