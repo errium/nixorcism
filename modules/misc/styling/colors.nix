b626c03c3cd3f9816dec155281470ad2c851abcf
Errium <197423581+Errium@users.noreply.github.com>
2026-01-19 19:29:20 +0200

    feat: cleanup color palettes

--- modules/misc/styling/colors.nix
+++ modules/misc/styling/colors.nix
@@ -1,44 +1,6 @@
 rec {
   palette = tokyo-night;
 
-  everforest = {
-    base00 = "#272e33";
-    base01 = "#2e383c";
-    base02 = "#414b50";
-    base03 = "#859289";
-    base04 = "#9da9a0";
-    base05 = "#d3c6aa";
-    base06 = "#edeada";
-    base07 = "#fffbef";
-    base08 = "#e67e80";
-    base09 = "#e69875";
-    base0A = "#dbbc7f";
-    base0B = "#a7c080";
-    base0C = "#83c092";
-    base0D = "#7fbbb3";
-    base0E = "#d699b6";
-    base0F = "#9da9a0";
-  };
-
-  nord = {
-    base00 = "#2e3440";
-    base01 = "#3b4252";
-    base02 = "#434c5e";
-    base03 = "#4c566a";
-    base04 = "#d8dee9";
-    base05 = "#e5e9f0";
-    base06 = "#eceff4";
-    base07 = "#8fbcbb";
-    base08 = "#bd616a";
-    base09 = "#d08770";
-    base0A = "#ebcb8b";
-    base0B = "#a3be8c";
-    base0C = "#88c0d0";
-    base0D = "#81a1c1";
-    base0E = "#b48ead";
-    base0F = "#5e81ac";
-  };
-
   oxocarbon = {
     base00 = "#161616";
     base01 = "#262626";
