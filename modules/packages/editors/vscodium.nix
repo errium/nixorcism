0d5f62d41338b9396ce1b92947436e3b40b77d47
Errium <197423581+Errium@users.noreply.github.com>
2026-01-24 12:53:40 +0200

    feat: add vscodium configuration with extensions and settings

--- modules/packages/editors/vscodium.nix
+++ modules/packages/editors/vscodium.nix
@@ -12,6 +12,40 @@
     hm.programs.vscode = {
       enable = true;
       package = pkgs.vscodium;
+
+      profiles.default = {
+        userSettings = {
+          "editor.formatOnSave" = true;
+          "editor.inlayHints.enabled" = "on";
+          "editor.inlayHints.fontSize" = 11;
+          "editor.lineNumbers" = "relative";
+          "editor.minimap.enabled" = false;
+          "editor.rulers" = [120];
+          "editor.scrollbar.horizontal" = "hidden";
+          "editor.scrollbar.horizontalScrollbarSize" = 0;
+          "editor.scrollbar.vertical" = "hidden";
+          "editor.scrollbar.verticalScrollbarSize" = 0;
+          "editor.stickyScroll.enabled" = true;
+          "editor.stickyScroll.maxLineCount" = 5;
+
+          "workbench.accounts.visible" = false;
+          "workbench.sideBar.location" = "right";
+          "workbench.sideBar.visible" = false;
+          "workbench.startupEditor" = "none";
+        };
+
+        extensions = with pkgs.vscode-extensions; [
+          # Utils
+          eamodio.gitlens
+          usernamehw.errorlens
+
+          # Go
+          golang.go
+
+          # Nix
+          jnoortheen.nix-ide
+        ];
+      };
     };
   };
 }
