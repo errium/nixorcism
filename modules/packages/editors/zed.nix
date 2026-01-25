c3db41605a52568eb359eeb9551b803c9c9f5e54
Errium <197423581+Errium@users.noreply.github.com>
2026-01-25 19:34:02 +0200

    feat: enhance Zed UI with minimal configuration

--- modules/packages/editors/zed.nix
+++ modules/packages/editors/zed.nix
@@ -26,12 +26,40 @@
       userSettings = {
         autosave = "on_focus_change";
         base_keymap = "VSCode";
-        format_on_save = "on";
+        collaboration_panel.button = false;
+        disable_ai = true;
         helix_mode = true;
+        project_panel.dock = "right";
         relative_line_numbers = "enabled";
+        scrollbar.show = "never";
+        terminal.button = false;
         theme = lib.mkIf config.stylix.enable "Base16 untitled";
         ui_font_size = lib.mkForce 16;
         wrap_guides = [120];
+
+        inlay_hints = {
+          enabled = true;
+          show_background = true;
+        };
+
+        toolbar = {
+          breadcrumbs = false;
+          quick_actions = false;
+          selections_menu = false;
+          agent_review = false;
+          code_actions = false;
+        };
+
+        title_bar = {
+          show_branch_icon = true;
+          show_branch_name = true;
+          show_menus = false;
+          show_onboarding_banner = false;
+          show_project_items = true;
+          show_sign_in = false;
+          show_user_menu = false;
+          show_user_picture = false;
+        };
 
         telemetry = {
           diagnostics = false;
