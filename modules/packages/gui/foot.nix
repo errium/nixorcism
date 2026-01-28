f535c148cd09b7e4054d62313ae9dbc449249112
Errium <197423581+Errium@users.noreply.github.com>
2026-01-28 12:53:06 +0200

    feat: add padding and improve foot terminal config

--- modules/packages/gui/foot.nix
+++ modules/packages/gui/foot.nix
@@ -11,17 +11,18 @@
     hm.programs.foot = {
       enable = true;
 
-      # NOTE
-      # Seems to be broken, 05-01-26
+      # NOTE: Seems to be broken, 05-01-26
       # server.enable = true;
 
       settings = {
+        main.pad = "20x20 center-when-maximized-and-fullscreen";
+        mouse.hide-when-typing = "yes";
+
         cursor = {
           blink = "yes";
           blink-rate = 1000;
           style = "beam";
         };
-        mouse.hide-when-typing = "yes";
       };
     };
   };
