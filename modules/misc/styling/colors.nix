891a0b81e4555eeea35f66cc3b81818589109cc4
Errium <197423581+Errium@users.noreply.github.com>
2026-02-13 14:10:48 +0200

    feat: revert to tokyo-night color palette

--- modules/misc/styling/colors.nix
+++ modules/misc/styling/colors.nix
@@ -1,5 +1,5 @@
 rec {
-  palette = fleet;
+  palette = tokyo-night;
 
   chalk = {
     base00 = "#151515";
@@ -18,82 +18,6 @@ rec {
     base0D = "#6fc2ef";
     base0E = "#e1a3ee";
     base0F = "#deaf8f";
-  };
-
-  fleet = {
-    base00 = "#181818";
-    base01 = "#1f1f1f";
-    base02 = "#292929";
-    base03 = "#898989";
-    base04 = "#c2c2c2";
-    base05 = "#d1d1d1";
-    base06 = "#efefef";
-    base07 = "#ffffff";
-    base08 = "#cc7c8a";
-    base09 = "#e09b70";
-    base0A = "#ebc88d";
-    base0B = "#a8cc7c";
-    base0C = "#82d2ce";
-    base0D = "#87c3ff";
-    base0E = "#af9cff";
-    base0F = "#e394dc";
-  };
-
-  jellybeans = {
-    base00 = "#151515";
-    base01 = "#1c1c1c";
-    base02 = "#404040";
-    base03 = "#888888";
-    base04 = "#a0a8b0";
-    base05 = "#e8e8d3";
-    base06 = "#e8e8d3";
-    base07 = "#f0f0f0";
-    base08 = "#cf6a4c";
-    base09 = "#ffb964";
-    base0A = "#fad07a";
-    base0B = "#99ad6a";
-    base0C = "#8fbfdc";
-    base0D = "#8197bf";
-    base0E = "#c6b6ee";
-    base0F = "#f0a0c0";
-  };
-
-  oxocarbon = {
-    base00 = "#161616";
-    base01 = "#262626";
-    base02 = "#393939";
-    base03 = "#525252";
-    base04 = "#dde1e6";
-    base05 = "#f2f4f8";
-    base06 = "#ffffff";
-    base07 = "#ffffff";
-    base08 = "#ee5396";
-    base09 = "#ff7eb6";
-    base0A = "#78a9ff";
-    base0B = "#42be65";
-    base0C = "#3ddbd9";
-    base0D = "#33b1ff";
-    base0E = "#be95ff";
-    base0F = "#82cfff";
-  };
-
-  paradise = {
-    base00 = "#151515";
-    base01 = "#1f1f1f";
-    base02 = "#2b2b2b";
-    base03 = "#424242";
-    base04 = "#b0abab";
-    base05 = "#e8e3e3";
-    base06 = "#e8e3e3";
-    base07 = "#f0f0f0";
-    base08 = "#b66467";
-    base09 = "#d9bc8c";
-    base0A = "#d9bc8c";
-    base0B = "#8c977d";
-    base0C = "#8aa6a2";
-    base0D = "#8da3b9";
-    base0E = "#a988b0";
-    base0F = "#b66467";
   };
 
   tokyo-night = {
