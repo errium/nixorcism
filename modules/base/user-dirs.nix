a418f6ebb786049fa90f4d3336629a846fff196f
Errium <197423581+Errium@users.noreply.github.com>
2026-02-21 14:45:34 +0200

    refactor: simplify user-dirs, improve helix theme template

--- modules/base/user-dirs.nix
+++ modules/base/user-dirs.nix
@@ -1,27 +1,29 @@
 {
   flake.modules.nixos.base = {
-    hm = {config, ...}: {
+    hm = {config, ...}: let
+      home = config.home.homeDirectory;
+    in {
       xdg.userDirs = {
         enable = true;
         createDirectories = true;
 
-        desktop = "${config.home.homeDirectory}/Desktop";
-        documents = "${config.home.homeDirectory}/Documents";
-        download = "${config.home.homeDirectory}/Downloads";
-        music = "${config.home.homeDirectory}/Music";
-        pictures = "${config.home.homeDirectory}/Pictures";
-        publicShare = "${config.home.homeDirectory}/Public";
-        templates = "${config.home.homeDirectory}/Templates";
-        videos = "${config.home.homeDirectory}/Videos";
+        desktop = "${home}/Desktop";
+        documents = "${home}/Documents";
+        download = "${home}/Downloads";
+        music = "${home}/Music";
+        pictures = "${home}/Pictures";
+        publicShare = "${home}/Public";
+        templates = "${home}/Templates";
+        videos = "${home}/Videos";
       };
 
       gtk.gtk3.bookmarks = [
-        "file://${config.home.homeDirectory}/Desktop"
-        "file://${config.home.homeDirectory}/Documents"
-        "file://${config.home.homeDirectory}/Downloads"
-        "file://${config.home.homeDirectory}/Music"
-        "file://${config.home.homeDirectory}/Pictures"
-        "file://${config.home.homeDirectory}/Videos"
+        "file://${home}/Desktop"
+        "file://${home}/Documents"
+        "file://${home}/Downloads"
+        "file://${home}/Music"
+        "file://${home}/Pictures"
+        "file://${home}/Videos"
       ];
     };
   };
--- modules/features/styling/matugen.nix
+++ modules/features/styling/matugen.nix
@@ -6,10 +6,10 @@
       enable = true;
 
       # TODO: Add a script for switching
-      wallpaper = ../../../assets/wallpaper.jpg; 
-      
+      wallpaper = ../../../assets/wallpaper.jpg;
+
       jsonFormat = "hex";
-      type = "scheme-tonal-spot";
+      # type = "scheme-tonal-spot";
       variant = "dark";
     };
   };
