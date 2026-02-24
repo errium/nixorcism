aed3bb49a065bf6d17a07a33b7c5795eb75bf130
Errium <197423581+Errium@users.noreply.github.com>
2026-02-24 17:10:43 +0200

    refactor: simplify sway host config

--- hosts/dudos-machine/host-specific/sway.nix
+++ hosts/dudos-machine/host-specific/sway.nix
@@ -3,14 +3,14 @@
     config,
     lib,
     ...
-  }: {
-    config = lib.mkIf config.programs.sway.enable {
-      hm.wayland.windowManager.sway.config = {
-        output = {
-          "BOE 0x0B38 0x00000003" = {
-            mode = ''2560x1600@165.002Hz'';
-            adaptive_sync = "on";
-          };
+  }: let
+    cfg = config.programs.sway.enable;
+  in {
+    hm.wayland.windowManager.sway.config = lib.mkIf cfg {
+      output = {
+        "BOE 0x0B38 0x00000003" = {
+          mode = ''2560x1600@165.002Hz'';
+          adaptive_sync = "on";
         };
       };
     };
