8f53897f094edda0490b642b52a0f5bbfba8a80b
Errium <197423581+Errium@users.noreply.github.com>
2026-06-22 14:20:06 +0300

    refactor: flatten nix settings nesting

--- modules/core/nix.nix
+++ modules/core/nix.nix
@@ -1,23 +1,21 @@
 {
   flake.modules.nixos.core = {
-    nix = {
-      settings = {
-        auto-optimise-store = true;
-        experimental-features = ["flakes" "nix-command" "pipe-operators"];
-        trusted-users = ["root" "@wheel"];
-      };
+    nix.settings = {
+      auto-optimise-store = true;
+      experimental-features = ["flakes" "nix-command" "pipe-operators"];
+      trusted-users = ["root" "@wheel"];
+    };
 
-      gc = {
-        # automatic = !config.hm.programs.nh.clean.enable;
-        dates = "daily";
-        options = "--delete-older-than 2d";
-      };
+    nix.gc = {
+      # automatic = !config.hm.programs.nh.clean.enable;
+      dates = "daily";
+      options = "--delete-older-than 2d";
+    };
 
-      optimise = {
-        automatic = true;
-        dates = "daily";
-        persistent = true;
-      };
+    nix.optimise = {
+      automatic = true;
+      dates = "daily";
+      persistent = true;
     };
 
     nixpkgs.config = {
