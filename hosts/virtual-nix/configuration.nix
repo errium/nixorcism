0260a841f3a610d75d987ee71c8d514f88feecd5
Errium <197423581+Errium@users.noreply.github.com>
2026-06-24 18:33:16 +0300

    feat: select latest ZFS-compatible kernel

--- hosts/virtual-nix/configuration.nix
+++ hosts/virtual-nix/configuration.nix
@@ -1,9 +1,31 @@
 {
-  flake.modules.nixos.virtual-nix = {pkgs, ...}: {
+  flake.modules.nixos.virtual-nix = {
+    config,
+    lib,
+    pkgs,
+    ...
+  }: let
+    # TEST
+    zfsCompatibleKernelPackages =
+      lib.filterAttrs (
+        name: kernelPackages:
+          (builtins.match "linux_[0-9]+_[0-9]+" name)
+          != null
+          && (builtins.tryEval kernelPackages).success
+          && (!kernelPackages.${config.boot.zfs.package.kernelModuleAttribute}.meta.broken)
+      )
+      pkgs.linuxKernel.packages;
+
+    latestZfsKernel = lib.last (
+      lib.sort (a: b: (lib.versionOlder a.kernel.version b.kernel.version)) (
+        builtins.attrValues zfsCompatibleKernelPackages
+      )
+    );
+  in {
     imports = [./_hardware.nix];
 
     system.stateVersion = "25.11";
-    boot.kernelPackages = pkgs.linuxPackages_latest;
+    boot.kernelPackages = latestZfsKernel;
 
     nixorcism.bootloader = "systemd-boot";
   };
