54bafee91fe0d5e06b7ac9ba5a0f67809a17c386
Errium <197423581+Errium@users.noreply.github.com>
2026-06-24 12:16:29 +0300

    fix: restructure install script with chown step

--- install.sh
+++ install.sh
@@ -1,16 +1,31 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
 # TEST:
 # Don't look here, this script isn't even a WIP.
 # Its sole purpose is to help me test some stuff by installing the system for me.
 
+# Partition and format
 sudo NIX_CONFIG="extra-experimental-features = pipe-operators" \
 	nix run 'github:nix-community/disko/latest' -- \
 	--mode destroy,format,mount \
-	--yes-wipe-all-disks \
 	--flake .#virtual-nix
 
-nixos-generate-config --root /mnt \
+# Generate hardware config
+nixos-generate-config \
 	--show-hardware-config \
-	--no-filesystems |
+	--no-filesystems \
+	--root /mnt |
 	tee ~/nixorcism/hosts/virtual-nix/_hardware.nix >/dev/null
 
-sudo nixos-install --flake .#virtual-nix --no-root-passwd
+# Install
+sudo NIX_CONFIG="extra-experimental-features = pipe-operators" \
+	nixos-install \
+	--no-root-passwd \
+	--flake .#virtual-nix
+
+# Copy config
+sudo mkdir -p /mnt/home/errium/nixorcism
+sudo cp -rT ~/nixorcism /mnt/home/errium/nixorcism
+sudo nixos-enter --root /mnt -c \
+	"chown -R errium:users /home/errium/nixorcism"
