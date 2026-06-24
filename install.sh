6393c4d639dc89a1408154bad4b3b4e1f3d0a8e4
Errium <197423581+Errium@users.noreply.github.com>
2026-06-24 11:33:16 +0300

    fix: restore full install pipeline with disko and nixos-install

--- install.sh
+++ install.sh
@@ -2,8 +2,15 @@
 # Don't look here, this script isn't even a WIP.
 # Its sole purpose is to help me test some stuff by installing the system for me.
 
-nixos-generate-config \
+sudo NIX_CONFIG="extra-experimental-features = pipe-operators" \
+	nix run 'github:nix-community/disko/latest' -- \
+	--mode destroy,format,mount \
+	--yes-wipe-all-disks \
+	--flake .#virtual-nix
+
+nixos-generate-config --root /mnt \
 	--show-hardware-config \
-	--no-filesystems \
-	--root /mnt |
+	--no-filesystems |
 	tee ~/nixorcism/hosts/virtual-nix/_hardware.nix >/dev/null
+
+sudo nixos-install --flake .#virtual-nix --no-root-passwd
