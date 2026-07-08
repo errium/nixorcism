ed7e4635c26f3e67c4d560ad8b59b549d3d333cb
Errium <197423581+Errium@users.noreply.github.com>
2026-07-09 00:04:38 +0300

    feat: add bash configuration with custom prompt

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -13,6 +13,21 @@
     };
 
     config = {
+      # bash
+      nixorcism.preserve.user.files = [".bash_history"];
+      programs.bash = {
+        enable = true;
+        completion.enable = true;
+        promptInit = ''
+          PS1='\[\e[1;2m\][ \[\e[0m\]'\
+          '\[\e[92;1m\]\u'\
+          '\[\e[39;2m\] · '\
+          '\[\e[0;1;96m\]\W'\
+          '\[\e[0m\] \[\e[1;2m\]]\[\e[0m\] '\
+          '\[\e[1;2m\]\$\[\e[0m\] '
+        '';
+      };
+
       # i18n
       i18n.defaultLocale = "en_US.UTF-8";
       time.timeZone = "Europe/Vilnius";
