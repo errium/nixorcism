f7d11502a303b76e24df3a6c41b951d75869cf81
Errium <197423581+Errium@users.noreply.github.com>
2026-07-12 14:10:01 +0300

    feat: improve network config with DNS fallback

--- modules/core/network.nix
+++ modules/core/network.nix
@@ -2,20 +2,23 @@
   flake.modules.nixos.core = {
     networking = {
       firewall.enable = true;
-      networkmanager.enable = true;
+      networkmanager = {
+        enable = true;
+        dns = "systemd-resolved";
+      };
     };
 
     services.resolved = {
       enable = true;
       settings.Resolve = {
-        DNS = [
-          "9.9.9.9#dns.quad9.net"
-          "1.1.1.1#cloudflare-dns.com"
-        ];
+        DNS = "9.9.9.9#dns.quad9.net";
+        FallbackDNS = "1.1.1.1#cloudflare-dns.com 8.8.8.8#dns.google";
         DNSOverTLS = "true";
         DNSSEC = "true";
         Domains = "~.";
       };
     };
+
+    nixorcism.preserve.directories = ["/etc/NetworkManager/system-connections"];
   };
 }
